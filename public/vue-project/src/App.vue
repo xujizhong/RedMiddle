a7f83d2ae956ca1fca223c42c70d4b2270b91009
xjz <1013633243@qq.com>
2019-01-23 15:53:39 +0800

    no message

--- public/vue-project/src/App.vue
+++ public/vue-project/src/App.vue
@@ -12,10 +12,8 @@ export default {
     return {
       tj: unescape(
         "%3Cspan id='cnzz_stat_icon_1276056134'%3E%3C/span%3E%3Cscript src='" +
-          document.location.protocol
-          ? "https://"
-          : "http://" +
-              "s23.cnzz.com/z_stat.php%3Fid%3D1276056134%26show%3Dpic' type='text/javascript'%3E%3C/script%3E"
+          (document.location.protocol ? "https://" : "http://") +
+          "s23.cnzz.com/z_stat.php%3Fid%3D1276056134%26show%3Dpic' type='text/javascript'%3E%3C/script%3E"
       )
     };
   }
