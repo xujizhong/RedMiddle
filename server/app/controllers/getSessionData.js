f4e58190fa158c4ba0223a8ff557092a6a919ce9
xjz <1013633243@qq.com>
2019-01-23 13:30:07 +0800

    no message

--- server/app/controllers/getSessionData.js
+++ server/app/controllers/getSessionData.js
@@ -3,7 +3,9 @@ const url = require('./file/url');
 const _config = require('../../config/config')
 
 module.exports = async (ctx) => {
-    if(ctx.session.user.avatar && ctx.session.user.avatar.indexOf(_config.qiniuUrl) < 0){
+    if (ctx.session.user &&
+        ctx.session.user.avatar &&
+        ctx.session.user.avatar.indexOf(_config.qiniuUrl) < 0) {
         ctx.session.user.avatar = url(ctx.session.user.avatar)
     }
     ctx.body = ctx.session.user;
