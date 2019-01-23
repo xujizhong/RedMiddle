a8535f8e4b9dbbe477b5903854fefd3c1873dce4
xjz <1013633243@qq.com>
2019-01-23 15:52:04 +0800

    no message

--- public/vue-project/src/App.vue
+++ public/vue-project/src/App.vue
@@ -1,11 +1,23 @@
 <template>
   <div id="app">
     <router-view/>
+    <div v-html="tj"></div>
   </div>
 </template>
 
 <script>
-export default{
-  name: 'App'
-}
+export default {
+  name: "App",
+  data() {
+    return {
+      tj: unescape(
+        "%3Cspan id='cnzz_stat_icon_1276056134'%3E%3C/span%3E%3Cscript src='" +
+          document.location.protocol
+          ? "https://"
+          : "http://" +
+              "s23.cnzz.com/z_stat.php%3Fid%3D1276056134%26show%3Dpic' type='text/javascript'%3E%3C/script%3E"
+      )
+    };
+  }
+};
 </script>
--- public/vue-project/src/views/main/Layer.vue
+++ public/vue-project/src/views/main/Layer.vue
@@ -51,15 +51,6 @@ const https = require("../../https/https");
 
 export default {
   created() {
-    var cnzz_protocol =
-      "https:" == document.location.protocol ? "https://" : "http://";
-    document.write(
-      unescape(
-        "%3Cspan id='cnzz_stat_icon_1276056134'%3E%3C/span%3E%3Cscript src='" +
-          cnzz_protocol +
-          "s23.cnzz.com/z_stat.php%3Fid%3D1276056134%26show%3Dpic' type='text/javascript'%3E%3C/script%3E"
-      )
-    );
     this.setUser();
   },
   methods: {
